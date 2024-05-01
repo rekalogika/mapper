0dcd8514141c54a909f387d2ce90dc19d86f8dd4
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-05-01 12:50:30 +0700

    fix: Setter does not get called if the property is also in the constructor

---
+++ tests/IntegrationTest/PropertyInSetterAndConstructorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/mapper package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Mapper\Tests\IntegrationTest;
+
+use Rekalogika\Mapper\Tests\Common\FrameworkTestCase;
+use Rekalogika\Mapper\Tests\Fixtures\PropertyInSetterAndConstructor\ChildObject;
+use Rekalogika\Mapper\Tests\Fixtures\PropertyInSetterAndConstructor\ChildObjectDto;
+use Rekalogika\Mapper\Tests\Fixtures\PropertyInSetterAndConstructor\ParentObject;
+use Rekalogika\Mapper\Tests\Fixtures\PropertyInSetterAndConstructor\ParentObjectDto;
+
+class PropertyInSetterAndConstructorTest extends FrameworkTestCase
+{
+    public function testIssue57(): void
+    {
+        $dto = new ParentObjectDto();
+        $dto->name = 'dto-name';
+        $dto->child = new ChildObjectDto();
+        $dto->child->a = 'dto-a';
+
+        $entity = $this->mapper->map($dto, ParentObject::class);
+
+        $this->assertSame('dto-name', $entity->getName());
+        $this->assertSame('dto-a', $entity->getChild()->getA());
+    }
+
+    public function testIssue57Preinitialized(): void
+    {
+        $dto = new ParentObjectDto();
+        $dto->name = 'dto-name';
+        $dto->child = new ChildObjectDto();
+        $dto->child->a = 'dto-a';
+
+        $entity =  new ParentObject('entity-name', new ChildObject('entity-a'));
+        $this->mapper->map($dto, $entity);
+
+        $this->assertSame('dto-name', $entity->getName());
+        $this->assertSame('dto-a', $entity->getChild()->getA());
+    }
+}
