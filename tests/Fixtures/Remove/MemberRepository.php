790880f56d4bbddb4764a6ad7f4ff8b55dea9a59
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-05-01 17:50:59 +0700

    feat: option to remove missing members from the target object

---
+++ tests/Fixtures/Remove/MemberRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/mapper package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Mapper\Tests\Fixtures\Remove;
+
+class MemberRepository
+{
+    /**
+     * @var array<string,Member>
+     */
+    private array $members = [];
+
+    public function add(string $id): Member
+    {
+        $member = new Member($id);
+        $this->members[$id] = $member;
+
+        return $member;
+    }
+
+    public function get(string $id): Member
+    {
+        return $this->members[$id] ?? throw new \InvalidArgumentException('Member not found');
+    }
+}
