9d8cd5ce594e571b2c151eb4d84c1b5ae0405b59
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-06-25 20:11:21 +0700

    fix: cache warming error on invalid class in transformer's `getSupportedTransformation` (#79)
    * fix: cache warming error on invalid class in transformer's `getSupportedTransformation`
    
    * bump psr/log dep

--- config/tests.php
+++ config/tests.php
@@ -11,8 +11,10 @@ declare(strict_types=1);
  * that was distributed with this source code.
  */
 
+use Psr\Log\LoggerInterface;
 use Rekalogika\Mapper\MapperInterface;
 use Rekalogika\Mapper\Tests\Common\TestKernel;
+use Rekalogika\Mapper\Tests\Fixtures\InvalidTransformer\InvalidTransformer;
 use Rekalogika\Mapper\Tests\Fixtures\Money\MoneyToMoneyDtoTransformer;
 use Rekalogika\Mapper\Tests\Fixtures\ObjectMapper\MoneyObjectMapper;
 use Rekalogika\Mapper\Tests\Fixtures\ObjectMapper\PersonToPersonDtoMapper;
@@ -26,6 +28,7 @@ use Rekalogika\Mapper\Tests\Fixtures\RememberingMapper\RememberingMapper;
 use Rekalogika\Mapper\Tests\Fixtures\Remove\MemberDtoToMemberMapper;
 use Rekalogika\Mapper\Tests\Fixtures\Remove\MemberRepository;
 use Rekalogika\Mapper\Tests\Fixtures\TransformerOverride\OverrideTransformer;
+use Rekalogika\Mapper\Tests\Service\TestLogger;
 use Rekalogika\Mapper\Transformer\Implementation\ScalarToScalarTransformer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -67,6 +70,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->tag('rekalogika.mapper.transformer')
         ->args([
             '$transformer' => service(ScalarToScalarTransformer::class),
+        ]);
+    $services->set(InvalidTransformer::class)
+        ->tag('rekalogika.mapper.transformer');
+
+    $services->set(TestLogger::class)
+        ->decorate(LoggerInterface::class)
+        ->args([
+            '$logger' => service('.inner'),
         ]);
 
     $services->set(RememberingMapper::class)
---
+++ src/Exception/InvalidClassException.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/mapper package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Mapper\Exception;
+
+class InvalidClassException extends InvalidArgumentException
+{
+    public function __construct(
+        private string $class,
+        int $code = 0,
+        ?\Throwable $previous = null
+    ) {
+        $message = sprintf('Class "%s" does not exist.', $class);
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+}
