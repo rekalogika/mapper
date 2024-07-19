d72cdb8d0f88c5ef4e792d7a3822ea9f980e52d8
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-07-19 10:10:51 +0700

    code quality 60

--- rector.php
+++ rector.php
@@ -40,7 +40,7 @@ return RectorConfig::configure()
         // symfonyCodeQuality: true,
         // doctrineCodeQuality: true,
     )
-    ->withCodeQualityLevel(57)
+    ->withCodeQualityLevel(60)
     ->withPhpSets(php82: true)
     ->withRules([
         AddOverrideAttributeToOverriddenMethodsRector::class,
