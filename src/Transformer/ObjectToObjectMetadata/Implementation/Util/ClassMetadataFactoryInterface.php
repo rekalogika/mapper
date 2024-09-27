b0abdbfb13d3dc38e509d899a94a2ba44627d9a2
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-09-27 14:32:29 +0700

    refactor: add general class metadata

---
+++ src/Transformer/ObjectToObjectMetadata/Implementation/Util/ClassMetadataFactoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/mapper package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Mapper\Transformer\ObjectToObjectMetadata\Implementation\Util;
+
+use Rekalogika\Mapper\Transformer\ObjectToObjectMetadata\Implementation\Model\ClassMetadata;
+
+/**
+ * @internal
+ */
+interface ClassMetadataFactoryInterface
+{
+    /**
+     * @param class-string $class
+     * @todo collect property path attributes
+     */
+    public function createClassMetadata(
+        string $class,
+    ): ClassMetadata;
+}
