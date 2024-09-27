899bff47571edaefe8192b1458ea345036f64f19
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-09-27 18:41:37 +0700

    test: immutable setter without setter on parent object error (#168)

--- src/Transformer/Exception/NewInstanceReturnedButCannotBeSetOnTargetException.php
+++ src/Transformer/Exception/NewInstanceReturnedButCannotBeSetOnTargetException.php
@@ -25,7 +25,7 @@ class NewInstanceReturnedButCannotBeSetOnTargetException extends NotMappableValu
     ) {
         parent::__construct(
             message: \sprintf(
-                'The existing target value of property "%s" on object type "%s" is a different instance from the new target value returned by the transformer. But the target object does not allow setting the value. This might indicate a limitation or bug in the transformer, custom object mapper, or property mapper. Or you can simply need to the setter method to the target object.',
+                'Transformation of property "%s" on object type "%s" results in a different object instance from the original instance, but the new instance cannot be set on the target object. You may wish to add a setter method to the target class.',
                 $propertyName,
                 get_debug_type($target),
             ),
